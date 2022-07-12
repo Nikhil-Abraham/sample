b20515958b94c69453573a57d5be6c8eb1fef669
Nikhil Abraham <nikhil.abraham@orangepower.co.uk>
2022-07-12 14:30:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, request, render_template
+from requests_html import HTMLSession
+import requests
+session = HTMLSession()
+
+
+app = Flask(__name__)
+
+
+def nre(text):
+
+    if 'https://www' not in text:
+        data = text
+    else:
+        x = session.get(text)
+        print(x.text)
+        data = x.text
+
+    return data
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'GET':
+        return render_template('home.html')
+
+    if request.method == 'POST':
+        data = request.form.get('test_input')
+        print(data)
+        return nre(data)
+
+
+@app.route('/predict')
+def predict():
+    return "this is the /predict page!"
+
+
+if __name__ == '__main__':
+    app.run()
